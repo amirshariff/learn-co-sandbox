97ea2872d076ba245658700c26ebeef6e83b5d1e
amir shariff <amirshariff@msn.com>
2020-12-16 14:41:57 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting
+  puts "hello"
+end
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+greeting
+say_greeting_five_times
---
+++ strings and numbers lab.js
@@ -0,0 +1,17 @@
+//JS Code Here
+var friends = document.querySelector('#friends');
+
+friends.innerhtml = parseInt(friends.innerhtml) + 1;
+
+friends = 14;
+
+console.log(friends);
+
+
+var employer = document.querySelector('#employer');
+
+employer.innerHTML='microsoft'
+
+console.log(employer);
+
+
